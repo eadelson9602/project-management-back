04103631198a7760a04c0fc67ddf512a21f3ea95
Elver Rodriguez <adelson9602@gmail.com>
2025-06-10 11:58:42 -0500

    Enable info api from package.json

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+interface PackageJson {
+  name: string;
+  description?: string;
+  author?: string;
+  version: string;
+  [key: string]: unknown;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getRootInfo() {
+    const packageJsonPath = join(__dirname, '..', '..', 'package.json');
+    const packageJson = JSON.parse(
+      readFileSync(packageJsonPath, 'utf8'),
+    ) as PackageJson;
+
+    return {
+      name: packageJson.name,
+      description: packageJson.description,
+      author: packageJson.author,
+      version: packageJson.version,
+    };
   }
 }

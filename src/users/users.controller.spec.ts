c1a9164e2ab9760414dd45bee8b88cceef8d47c8
Elver Rodriguez <adelson9602@gmail.com>
2025-06-10 00:55:19 -0500

    configure basic test

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'node:test';
+
+describe('UsersController', () => {
+  it('should return a list of users', () => {
+    console.log('Testing UsersController findAll method');
+  });
+});
